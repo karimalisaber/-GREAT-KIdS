665f13968396ba03b44155b617b53ecc63f822fa
unknown <karimali201094@gmail.com>
2020-06-25 03:35:48 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Lightbox } from 'ngx-lightbox';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hadana';
+  portfolioImages:  Array<any> = []; 
+  portfolioImagesWindow:  Array<any> = []; 
+  url ='assets/images/portfolio/'; 
+  title = 'GREAT KIdS';
+
+  kairm;
+  
+
+  constructor(private _lightbox: Lightbox){
+    // this.setPortfolio();
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.setPortfolio();
+    
+  }
+  setPortfolio(){
+    for(let i = 1; i<44; i++){
+      if(i<13)
+        this.portfolioImages.push( this.url + i +'.jpg');
+      this.portfolioImagesWindow.push( {'src': this.url + i +'.jpg'});      
+    };
+  }
+
+  
+  open(i){
+    this._lightbox.open(this.portfolioImagesWindow, 1);
+
+    // let postsImgs =[];
+    // imgs.filter(res=> postsImgs.push({'src': this.url + res.name}))
+   
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LightboxModule } from 'ngx-lightbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormField } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LightboxModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
